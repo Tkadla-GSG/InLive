f97b2ee3af5a7569831497071d733c48f0b67da7
J.T <tkadlecek.j@gmail.com>
2014-09-16 23:09:07 +0200

    NEW: action bar

--- app/src/main/java/cz/inlive/inlive/activity/LandingPageActivity.java
+++ app/src/main/java/cz/inlive/inlive/activity/LandingPageActivity.java
@@ -20,9 +20,12 @@ import android.view.View;
 import android.view.ViewGroup;
 
 import cz.inlive.inlive.R;
+import cz.inlive.inlive.fragment.BetFragment;
+import cz.inlive.inlive.fragment.HistoryFragment;
+import cz.inlive.inlive.listeners.BetFragmentInteractionListener;
 
 
-public class LandingPageActivity extends Activity implements ActionBar.TabListener {
+public class LandingPageActivity extends Activity implements ActionBar.TabListener, BetFragmentInteractionListener {
 
     /**
      * The {@link android.support.v4.view.PagerAdapter} that will provide
@@ -103,9 +106,9 @@ public class LandingPageActivity extends Activity implements ActionBar.TabListen
         // automatically handle clicks on the Home/Up button, so long
         // as you specify a parent activity in AndroidManifest.xml.
         int id = item.getItemId();
-        if (id == R.id.action_settings) {
+        /*if (id == R.id.action_settings) {
             return true;
-        }
+        }*/
         return super.onOptionsItemSelected(item);
     }
 
@@ -124,6 +127,11 @@ public class LandingPageActivity extends Activity implements ActionBar.TabListen
     public void onTabReselected(ActionBar.Tab tab, FragmentTransaction fragmentTransaction) {
     }
 
+    @Override
+    public void onFragmentInteraction(String s) {
+        //nothing to do
+    }
+
     /**
      * A {@link FragmentPagerAdapter} that returns a fragment corresponding to
      * one of the sections/tabs/pages.
@@ -137,14 +145,25 @@ public class LandingPageActivity extends Activity implements ActionBar.TabListen
         @Override
         public Fragment getItem(int position) {
             // getItem is called to instantiate the fragment for the given page.
-            // Return a PlaceholderFragment (defined as a static inner class below).
-            return PlaceholderFragment.newInstance(position + 1);
+
+            switch(position){
+
+                case 0:
+                    return  new BetFragment().newInstance("dummy", "dummy");
+
+                case 1:
+                    return new HistoryFragment().newInstance("dummy", "dummy");
+
+                default:
+                    return  new BetFragment().newInstance("dummy", "dummy");
+
+            }
         }
 
         @Override
         public int getCount() {
-            // Show 3 total pages.
-            return 3;
+            // Show 2 total pages.
+            return 2;
         }
 
         @Override
@@ -155,43 +174,8 @@ public class LandingPageActivity extends Activity implements ActionBar.TabListen
                     return getString(R.string.title_section1).toUpperCase(l);
                 case 1:
                     return getString(R.string.title_section2).toUpperCase(l);
-                case 2:
-                    return getString(R.string.title_section3).toUpperCase(l);
             }
             return null;
-        }
-    }
-
-    /**
-     * A placeholder fragment containing a simple view.
-     */
-    public static class PlaceholderFragment extends Fragment {
-        /**
-         * The fragment argument representing the section number for this
-         * fragment.
-         */
-        private static final String ARG_SECTION_NUMBER = "section_number";
-
-        /**
-         * Returns a new instance of this fragment for the given section
-         * number.
-         */
-        public static PlaceholderFragment newInstance(int sectionNumber) {
-            PlaceholderFragment fragment = new PlaceholderFragment();
-            Bundle args = new Bundle();
-            args.putInt(ARG_SECTION_NUMBER, sectionNumber);
-            fragment.setArguments(args);
-            return fragment;
-        }
-
-        public PlaceholderFragment() {
-        }
-
-        @Override
-        public View onCreateView(LayoutInflater inflater, ViewGroup container,
-                Bundle savedInstanceState) {
-            View rootView = inflater.inflate(R.layout.fragment_landing_page, container, false);
-            return rootView;
         }
     }
 
---
+++ app/src/main/java/cz/inlive/inlive/listeners/BetFragmentInteractionListener.java
@@ -0,0 +1,11 @@
+package cz.inlive.inlive.listeners;
+
+import android.net.Uri;
+
+/**
+ * Created by Tkadla on 15. 9. 2014.
+ */
+public interface BetFragmentInteractionListener {
+    // TODO: Update argument type and name
+    public void onFragmentInteraction(String s);
+}
